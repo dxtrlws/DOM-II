573c29bc9238d98eb397f817ca4a75d9c9e941d1
Dexter Lewis <dexter.lewis@gmail.com>
2019-02-02 10:32:15 -0500

    add mouseover and dblclick requirements

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+
+// Mouseover - change background color of all buttons
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach( button => {
+    button.addEventListener('mouseover', e => {
+        button.style.backgroundColor = 'red';
+    })
+});
+
+// Dblclick - toggle images in content section
+const domImages = document.querySelectorAll('.img-content img');
+domImages.forEach(domImage => {
+    domImage.addEventListener('dblclick', e => {
+        if (domImage.getAttribute('src') === 'img/fun.jpg') {
+            domImage.setAttribute('src', 'img/adventure.jpg');
+        } else {
+            domImage.setAttribute('src','img/fun.jpg');
+        }
+    })
+})
+
+
