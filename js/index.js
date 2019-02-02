c9b0b1f628aec7dff65851bb1bd48297673cd2d6
Dexter Lewis <dexter.lewis@gmail.com>
2019-02-02 11:20:04 -0500

    add scroll event

--- js/index.js
+++ js/index.js
@@ -1,35 +1,40 @@
 // Your code goes here
 
 // Mouseover - change background color of all buttons
-const buttons = document.querySelectorAll('.btn');
-buttons.forEach( button => {
-    button.addEventListener('mouseover', e => {
-        button.style.backgroundColor = 'red';
-    })
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach(button => {
+  button.addEventListener("mouseover", e => {
+    e.target.style.backgroundColor = "red";
+  });
 });
 
 // Dblclick - toggle images in content section
-const domImages = document.querySelectorAll('.img-content img');
+const domImages = document.querySelectorAll(".img-content img");
 domImages.forEach(domImage => {
-    domImage.addEventListener('dblclick', e => {
-        if (domImage.getAttribute('src') === 'img/fun.jpg') {
-            domImage.setAttribute('src', 'img/adventure.jpg');
-        } else {
-            domImage.setAttribute('src','img/fun.jpg');
-        }
-    })
+  domImage.addEventListener("dblclick", e => {
+    e.target.getAttribute("src") === "img/fun.jpg"
+      ? e.target.setAttribute("src", "img/adventure.jpg")
+      : e.target.setAttribute("src", "img/fun.jpg");
+  });
 });
 
 // Resize - change background color of paragraph
-window.addEventListener('resize',() => {
-    const contentDestination = document.querySelector('.content-destination p');
-    contentDestination.style.color = 'rgb('+random(0,255)+','+random(0,255)+','+random(0,255)+')';
-} )
+window.addEventListener("resize", () => {
+  const contentDestination = document.querySelector(".content-destination p");
+  contentDestination.style.color =
+    "rgb(" + random(0, 255) + "," + random(0, 255) + "," + random(0, 255) + ")";
+});
 
-const random = (min, max) =>{
-    let rand = min + Math.random()*(max+1-min);
-    rand = rand^0; 
-    return rand;
-  }
+// Scroll
+window.addEventListener("scroll", () => {
+  const intro = document.querySelector(".intro p");
+  intro.style.color =
+    "rgb(" + random(0, 255) + "," + random(0, 255) + "," + random(0, 255) + ")";
+});
 
-  
+// helper function
+const random = (min, max) => {
+  let rand = min + Math.random() * (max + 1 - min);
+  rand = rand ^ 0;
+  return rand;
+};
